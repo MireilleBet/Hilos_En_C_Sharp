e16987978bb0a19c4603a2e6e3a0fc439a31d141
MireilleBet <85461751+MireilleBet@users.noreply.github.com>
2021-07-10 12:11:44 -0500

    Add files via upload

---
+++ SumarThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HilosEnCSharp
+{
+    class SumarThread
+    {
+        public void sumar(Object parametros)
+        {
+            Thread current = Thread.CurrentThread;
+
+            Int32[] numeros = (Int32[])parametros;
+            Int32 resultado = numeros[0] + numeros[1];
+            Console.WriteLine("El hilo " + current.Name + "durmiendo por" + resultado +
+               "segundos");
+            Thread.Sleep(resultado * 1000);
+            Console.WriteLine("El resultado de la suma es" +
+                numeros[0] + "+" + numeros[1] + "=" + resultado);
+            Console.WriteLine("Hilo" + current.Name + "finalizó su trabajo");
+        }
+    }
+}
